4eb9a81d82848f86669abdb00109e344f6b815e1
Your Nameq <ay05386@gmail.com>
2023-12-06 14:40:14 +0200

    fifth

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        System.out.println("lets go ");
+        System.out.println("fourth");
+        System.out.println("fifth");
     }
 }
